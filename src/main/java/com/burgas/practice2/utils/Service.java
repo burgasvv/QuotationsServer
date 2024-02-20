3c9938b953269051b1ff1ef7d3d5c5a0dc846686
Burgas <burgassme@gmail.com>
2024-02-20 17:01:11 +0700

    Created a method "info" that displays the class canonical name in the console in branch feature/task2.

--- src/main/java/com/burgas/practice2/utils/Service.java
+++ src/main/java/com/burgas/practice2/utils/Service.java
@@ -1,5 +1,10 @@
 package com.burgas.practice2.utils;
 
+import static java.lang.System.*;
+
 public class Service {
 
+    public void info(){
+        out.println(getClass().getCanonicalName());
+    }
 }
